af9e86e05fef484f0ed556adac0d245429c82202
Vitória Jacó <vitoriajaco@gmail.com>
2022-01-14 17:52:15 -0300

    implementar metodo para gerar valor aleatorio

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
 const imagem = document.querySelector('img');
 const botao = document.querySelector('button');
-const nomeDoPersonagem = document.querySelector('#Nome')
-const especie = document.querySelector("Especie")
-const status = document.querySelector("Status")
+const nomeDoPersonagem = document.querySelector('#Nome');
+const especie = document.querySelector("Especie");
+const condicao = document.querySelector("Status");
+
+gerarValorAleatorio =() => {
+    return Math.floor(Math.random() * 671);
+}
+
+pegarPersonagem = () => {
+    let numeroAleatorio = gerarValorAleatorio();
+    return fetch(`https://rickandmortyapi.com/api/character/${numeroAleatorio}` , {
+        method: 'GET',
+        headers: {
+            Accept: 'application/json',
+            "Content-type": 'application/json'
+        }
+    }).then((Response) => response.json()).then((data) => {
+        imagem.src = data.image;
+        imagem.alt= data.name;
+        nomeDoPersonagem.innerHTML = data.name;
+        especie.innerHTML = data.species;
+        condicao.innerHTML = data.status;
+    });
+}
+
+botao.onclick = pegarPersonagem;
