c3d9e6c36210a1a46ee77e6b8bf4165c47f999e0
Vitória Jacó <vitoriajaco@gmail.com>
2022-01-14 16:53:39 -0300

    Adicionar interatividade ao botao

---
+++ script.js
@@ -0,0 +1,5 @@
+const imagem = document.querySelector('img');
+const botao = document.querySelector('button');
+const nomeDoPersonagem = document.querySelector('#Nome')
+const especie = document.querySelector("Especie")
+const status = document.querySelector("Status")
