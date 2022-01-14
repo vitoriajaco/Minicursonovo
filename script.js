9d4770ffb10bb47f1ad193712915e2bf7a4e53cf
Vitória Jacó <vitoriajaco@gmail.com>
2022-01-14 19:56:10 -0300

    Qualquer coisa

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 const imagem = document.querySelector('img');
 const botao = document.querySelector('button');
-const nomeDoPersonagem = document.querySelector('#Nome');
-const especie = document.querySelector("Especie");
-const condicao = document.querySelector("Status");
+const nomeDoPersonagem = document.querySelector('#nome');
+const especie = document.querySelector("#especie");
+const condicao = document.querySelector("#status");
+
+traduzirCondicao = (data) => {
+    if(data.status =='unknown'){
+        return 'Não sabemos';
+    }else if(data.status =='Alive'){
+        return 'Sim';
+    }else {
+        return 'Não. Está morto';
+    
+    }
+}
 
 gerarValorAleatorio =() => {
     return Math.floor(Math.random() * 671);
@@ -16,7 +27,7 @@ pegarPersonagem = () => {
             Accept: 'application/json',
             "Content-type": 'application/json'
         }
-    }).then((Response) => response.json()).then((data) => {
+    }).then((response) => response.json()).then((data) => {
         imagem.src = data.image;
         imagem.alt= data.name;
         nomeDoPersonagem.innerHTML = data.name;
